86e0f5d156a166e7a1e709a8496ef3f660b969ca
24chris <40150356+24chris@users.noreply.github.com>
2022-12-23 17:55:27 +0300

    Update authcontext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -22,7 +22,7 @@ export const AuthProvider = ({children}) => {
 
     let loginUser = async (e )=> {
         e.preventDefault()
-        let response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/token/`, {
+        let response = await fetch('https://fieldtest.owinoonline.com/api/token/', {
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
@@ -59,7 +59,7 @@ export const AuthProvider = ({children}) => {
 
     let updateToken = async ()=> {
 
-        let response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/token/refresh/`, {
+        let response = await fetch('https://fieldtest.owinoonline.com/api/token/refresh/', {
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
