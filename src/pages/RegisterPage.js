9dd4f22c38c3b18f07ac5e0893b7c803098cc7c5
24chris <40150356+24chris@users.noreply.github.com>
2022-12-23 18:00:12 +0300

    Update Register Page

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -25,7 +25,7 @@ const Register = () => {
   let register = async e=> {
     e.preventDefault()
     const newuser = {email,username,password,role}
-    let response = await fetch('http://http://54.225.179.21/api/register/', {
+    let response = await fetch('https://fieldtest.owinoonline.com/api/register/', {
         method:'POST',
         headers:{
             'Content-Type':'application/json',
