5fff66d0a42ec8ec4137b6411a18aabad489a209
24chris <40150356+24chris@users.noreply.github.com>
2023-01-25 10:57:43 +0300

    omit- flutterwave

--- src/components/MultiStepForm/RegForm.js
+++ src/components/MultiStepForm/RegForm.js
@@ -62,25 +62,25 @@ const RegForm = () => {
 // },[])
 
 
-const config = {
-  public_key:'FLWPUBK-6c66fbf331a89f824d6d5b164088a8a6-X',
-  tx_ref: Date.now(),
-  amount: 10000,
-  currency: 'UGX',
-  payment_options: 'card,mobilemoneyuganda',
-  customer: {
-    email: 'test@gmail.com',
-    phonenumber: {mm_unber},
-    name: 'Peter',
-  },
-  customizations: {
-    title: 'Registration fee',
-    description: 'Payment for Registration',
-    logo: '#',
-  },
-};
+// const config = {
+//   public_key:'FLWPUBK-6c66fbf331a89f824d6d5b164088a8a6-X',
+//   tx_ref: Date.now(),
+//   amount: 10000,
+//   currency: 'UGX',
+//   payment_options: 'card,mobilemoneyuganda',
+//   customer: {
+//     email: 'test@gmail.com',
+//     phonenumber: {mm_unber},
+//     name: 'Peter',
+//   },
+//   customizations: {
+//     title: 'Registration fee',
+//     description: 'Payment for Registration',
+//     logo: '#',
+//   },
+// };
 
-const handleFlutterPayment = useFlutterwave(config);
+// const handleFlutterPayment = useFlutterwave(config);
 
   
 
